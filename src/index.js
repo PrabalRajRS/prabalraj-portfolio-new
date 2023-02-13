8ebad242e55bc7344300b54f17ac2b4bd00c2fa4
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2023-02-13 23:19:00 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { render } from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App'
 import { ThemeProvider } from './contexts/theme'
 import './index.css'
 
-render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ThemeProvider>
+  <App />
+</ThemeProvider>);

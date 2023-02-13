7a0e83124784d2f7247be2c98d34c4fd9df55bcd
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2023-02-14 01:21:43 +0530

    hello

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -4,18 +4,18 @@ import PropTypes from 'prop-types'
 const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
-  const [themeName, setThemeName] = useState('light')
+  const [themeName, setThemeName] = useState('dark')
 
   useEffect(() => {
     const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
+    setThemeName(darkMediaQuery.matches ? 'light' : 'dark')
     darkMediaQuery.addEventListener('change', (e) => {
-      setThemeName(e.matches ? 'dark' : 'light')
+      setThemeName(e.matches ? 'light' : 'dark')
     });
   }, [])
 
   const toggleTheme = () => {
-    const name = themeName === 'dark' ? 'light' : 'dark'
+    const name = themeName === 'light' ? 'dark' : 'light'
     localStorage.setItem('themeName', name)
     setThemeName(name)
   }

e528a5f7e66f3b576f398ec42491d3d21f1c9f96
PRABAL RAJ <96192793+PrabalRajRS@users.noreply.github.com>
2024-06-25 12:49:17 +0530

    recent changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,4 +1,4 @@
-import file from './files/prabal-raj-resume.pdf';
+import file from './files/prabal-raj-resume-final.pdf';
 const header = {
   // all the properties are optional - can be left empty or deleted
   // homepage: 'https://rajshekhar26.github.io/cleanfolio',
@@ -9,7 +9,7 @@ const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Prabal Raj',
   role: 'React Js Developer',
-  description: '2+ years of experience in React JS and front-end development. Proficient in designing user interfaces and building dynamic web apps. Skilled in HTML, CSS, JavaScript, and React libraries. Strong debugging and optimization skills, focused on delivering top-notch user experiences.',
+  description: '3+ years of experience in React JS and front-end development. Proficient in designing user interfaces and building dynamic web apps. Skilled in HTML, CSS, JavaScript, and React libraries. Strong debugging and optimization skills, focused on delivering top-notch user experiences.',
   resume: file,
   social: {
     linkedin: 'https://www.linkedin.com/in/prabal-raj-r-b2ba44204/',
@@ -56,7 +56,23 @@ const projects = [
     name: 'College ERP',
     description:
       'This application is used for managing and organizing college staffs, Admin, Director, Students data.',
-    stack: ['React', 'Redux', 'React Bootstrap', 'Node.js'],
+    stack: ['React', 'Redux', 'Material UI', 'Node.js'],
+    // sourceCode: 'https://github.com',
+    // livePreview: 'https://github.com',
+  },
+  {
+    name: 'Analogyx BI',
+    description:
+      'This application is used for managing businesses and to analyze and visualize their data in different forms to gain insights and make informed decisions.',
+    stack: ['React', 'Redux', 'React Bootstrap', 'Ant design', 'Sockets', 'Luckysheet'],
+    // sourceCode: 'https://github.com',
+    // livePreview: 'https://github.com',
+  },
+  {
+    name: 'Woodland Warehouse App',
+    description:
+      'An efficient Warehouse Application to streamline inventory control and logistics, significantly improving operational efficiency and accuracy.',
+    stack: ['React Native', 'Redux'],
     // sourceCode: 'https://github.com',
     // livePreview: 'https://github.com',
   },
